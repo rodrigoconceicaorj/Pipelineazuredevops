1c400d8d4cc013fb6496e1fac8d25b21e5266f95
rodrigoconceicaorj <120535995+rodrigoconceicaorj@users.noreply.github.com>
2023-10-31 18:58:46 -0300

    Add files via upload

---
+++ devops/deploy_templates/deploy_scripts/AKS/helm-upgrade.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ${SYSTEM_DEFAULTWORKINGDIRECTORY}/_${BUILD_DEFINITIONNAME}/${BUILD_DEFINITIONNAME}/helm_charts/$AMBIENTE
+
+docker pull dockerrod.com.br/fabrica/devops/kubernetes/helm-chart:2.6
+
+docker run -i --rm \
+-e "CLIENT_ID=$CLIENT_ID" \
+-e "SCOPE=$SCOPE" \
+-e "CLIENT_SECRET=$CLIENT_SECRET" \
+-e "TENANT_ID=$TENANT_ID" \
+-e "URL_KEYVAULT=$URL_KEYVAULT" \
+-e "PROJECT=$PROJECT" \
+-e "HELM_RELEASE_NAME"=$HELM_RELEASE_NAME \
+-e "CLUSTER=$CLUSTER" \
+-e "AMBIENTE=$AMBIENTE" \
+-v $(pwd):/opt/app \
+dockerrod.com.br/fabrica/devops/kubernetes/helm-chart:2.6
